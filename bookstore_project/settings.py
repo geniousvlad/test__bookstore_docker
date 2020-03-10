23a10ef46f5d8831af8a627fe69f72389b94a9d3
Vlad Kuzmin <devlad@bk.ru>
2020-03-10 20:53:51 +0200

    checked_settingspy

--- bookstore_project/settings.py
+++ bookstore_project/settings.py
@@ -176,8 +176,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'vladislavbossofbosses@gmail.com'
-EMAIL_HOST_PASSWORD = 'pdqbxaajlvlnaalo'
+EMAIL_HOST_USER = [username]
+EMAIL_HOST_PASSWORD = [password]
 
 ACCOUNT_SESSION_REMEMBER = True
 
@@ -191,7 +191,7 @@ ACCOUNT_EMAIL_REQUIRED = True
 
 ACCOUNT_UNIQUE_EMAIL = True
 
-DEFAULT_FROM_EMAIL = 'admin@djangobookstore.com'
+DEFAULT_FROM_EMAIL = [from_email]
 
 # Stripe
 STRIPE_TEST_PUBLISHABLE_KEY=os.environ.get('STRIPE_TEST_PUBLISHABLE_KEY')
